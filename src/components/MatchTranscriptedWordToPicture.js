48368ea7cf055366157a4ae9f5860363761595f9
nazimkazim <nazik_111@mail.ru>
2020-04-07 12:19:29 +0600

    add start again functionality

--- src/components/MatchTranscriptedWordToPicture.js
+++ src/components/MatchTranscriptedWordToPicture.js
@@ -17,7 +17,8 @@ class MatchTranscriptedWordToPicture extends Component {
             data: '',
             wordIndex: 0,
             disabled: false,
-            nameIsCorrect: false
+            nameIsCorrect: false,
+            activityIsFinished: false
         };
 
 
@@ -63,7 +64,7 @@ class MatchTranscriptedWordToPicture extends Component {
     }
 
 
-    
+
 
     dashedWord(str1, str2) {
         let str = str1 + ", " + str2;
@@ -138,75 +139,105 @@ class MatchTranscriptedWordToPicture extends Component {
             this.setState({
                 nameIsCorrect: true, index: this.state.index + 1
             });
-        this.update()
+            this.update();
+            if (this.state.index === this.state.data.length - 1) {
+                this.setState({
+                    activityIsFinished: true, index: 0
+                });
+            }
+
         } else {
+
             let audio = new Audio(denied);
             audio.play();
+
         }
+
+
     };
 
     update() {
         let dashedName = [];
         this.props.data.map((item) => {
-            return ( dashedName.push(this.dashedWord(item.rusName, item.companyRus)));
+            return (dashedName.push(this.dashedWord(item.rusName, item.companyRus)));
         });
         this.setState({
-            nameGuessed: dashedName[this.state.index+1],wordIndex:0, disabled:false
+            nameGuessed: dashedName[this.state.index + 1], wordIndex: 0, disabled: false, nameIsCorrect: false
+        });
+    }
+
+    startAgain = () => {
+        console.log("cliked")
+        let dashedName = [];
+        this.props.data.map((item) => {
+            return (dashedName.push(this.dashedWord(item.rusName, item.companyRus)));
+        });
+        this.setState({
+            index: 0, nameGuessed: dashedName[this.state.index], wordIndex: 0, disabled: false, nameIsCorrect: false, activityIsFinished:false
         });
     }
 
     render() {
         //console.log(this.state.nameGuessed.length === this.state.wordIndex);
         //console.log(this.state.nameGuessed, this.state.rusName);
-        console.log(this.state.nameGuessed);
-        console.log(this.state.nameGuessed === this.state.data[this.state.index] && this.state.data[this.state.index].correctAnswer.trim(), this.state.nameGuessed, this.state.data[this.state.index] && this.state.data[this.state.index].correctAnswer.trim());
-
+        //console.log(this.state.nameGuessed);
+        //console.log(this.state.nameGuessed === this.state.data[this.state.index] && this.state.data[this.state.index].correctAnswer.trim(), this.state.nameGuessed, this.state.data[this.state.index] && this.state.data[this.state.index].correctAnswer.trim());
+        console.log(this.state.index, this.state.data.length);
         return (
             <div className="columns is-multiline is-vcentered">
                 { this.props.data &&
                     <div className="column is-3">
-                        <div className="card">
-                            <div className="card-image">
-                                <figure className="image is-4by3">
-                                    <img src={ this.props.data[this.state.index].picture } alt="" />
-                                </figure>
-                            </div>
-                            <div className="card-content">
-                                <div className="media">
-                                    <div className="media-content">
-                                        <p className="title is-4">{ this.props.data[this.state.index].engName }</p>
-                                        <p className="subtitle is-6">{ this.props.data[this.state.index].companyEng }</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card-content">
-                                <div className="media">
-                                    <div className="media-content">
-                                        <p className="title is-4">{ this.state.nameGuessed && this.state.nameGuessed }
-                                            <button className="button is-small" onClick={ this.removeLetters } disabled={ this.state.wordIndex <= 0 }>
-                                                <span className="icon is-small">
-                                                    <i className="fas fa-backspace"></i>
-                                                </span>
-                                            </button>
-                                        </p>
-                                        { this.state.disabled && (<button onClick={ this.checkAnswer } className={ `button is-small ${this.state.nameIsCorrect && 'is-success'}` }>
-                                            <span class="icon is-small">
-                                                <i class="fas fa-check-circle"></i>
-                                            </span>
-                                        </button>) }
-                                        <p className="subtitle is-6"></p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="card-content">
-                                <div className="media">
-                                    <div className="media-content">
-                                        <div className="tags">{ this.state.data[this.state.index] && this.state.data[this.state.index].scrambleLetters.map((letter) => (
-                                            <button disabled={ this.state.disabled } className="tag is-dark is-light is-medium" value={ letter } onClick={ this.handleGuess } styles={ { cursor: 'pointer' } }>{ letter }</button>
-                                        )) }</div>
-                                    </div>
-                                </div>
-                            </div>
+                        <div className="card mtwp-card-container">
+                            { this.state.activityIsFinished ? (
+                                <>
+                                    <div className="mtwp-winner-title">You are winner</div>
+                                    <button className="button" onClick={ this.startAgain }>Again</button>
+                                </>) : (
+                                    <>
+                                        <div className="card-image">
+                                            <figure className="image is-4by3">
+                                                <img src={ this.props.data[this.state.index].picture } alt="" />
+                                            </figure>
+                                        </div>
+                                        <div className="card-content">
+                                            <div className="media">
+                                                <div className="media-content">
+                                                    <p className="title is-4">{ this.props.data[this.state.index].engName }</p>
+                                                    <p className="subtitle is-6">{ this.props.data[this.state.index].companyEng }</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="card-content">
+                                            <div className="media">
+                                                <div className="media-content">
+                                                    <p className="title is-4">{ this.state.nameGuessed && this.state.nameGuessed }
+                                                        <button className="button is-small" onClick={ this.removeLetters } disabled={ this.state.wordIndex <= 0 }>
+                                                            <span className="icon is-small">
+                                                                <i className="fas fa-backspace"></i>
+                                                            </span>
+                                                        </button>
+                                                    </p>
+                                                    { this.state.disabled && (<button onClick={ this.checkAnswer } className={ `button is-small ${this.state.nameIsCorrect && 'is-success'}` }>
+                                                        <span className="icon is-small">
+                                                            <i className="fas fa-check-circle"></i>
+                                                        </span>
+                                                    </button>) }
+                                                    <p className="subtitle is-6"></p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="card-content">
+                                            <div className="media">
+                                                <div className="media-content">
+                                                    <div className="tags">{ this.state.data[this.state.index] && this.state.data[this.state.index].scrambleLetters.map((letter) => (
+                                                        <button disabled={ this.state.disabled } className="tag is-dark is-light is-medium" value={ letter } onClick={ this.handleGuess } styles={ { cursor: 'pointer' } }>{ letter }</button>
+                                                    )) }</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </>
+                                ) }
+
                         </div>
                     </div>
                 }

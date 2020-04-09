07ef0461c712fd7ab835a170e67f2643a20b66da
nazimkazim <nazik_111@mail.ru>
2020-04-09 19:06:06 +0600

    add shuffle to input data

--- src/data/ComposeOrderedSentOnClick.js
+++ src/data/ComposeOrderedSentOnClick.js
@@ -1,18 +1,29 @@
-export const set1COSOC = [
+let _ = require("lodash");
+
+
+export const set1COSOC = _.shuffle([
     {
-        eng:"This is Larry Page, he is from Google",
-        rus:"Это Ларри Пейдж, он из Гугл"
+        eng: "This is Larry Page, he is from Google",
+        rus: "Это Ларри Пейдж, он из Гугл"
     },
     {
-        eng:"This is Jeff Bezos, he is from Amazon",
-        rus:"Это Джеф Безос, он из Амазон"
+        eng: "This is Jeff Bezos, he is from Amazon",
+        rus: "Это Джеф Безос, он из Амазон"
     },
     {
-        eng:"This is Tim Cook, he is from Apple",
-        rus:"Это Тим Кук, он из Аппл"
+        eng: "This is Tim Cook, he is from Apple",
+        rus: "Это Тим Кук, он из Аппл"
     },
     {
-        eng:"This is Bill Gates, he is from Microsoft",
-        rus:"Это Билл Гейтс, он из Майкрософт"
+        eng: "This is Bill Gates, he is from Microsoft",
+        rus: "Это Билл Гейтс, он из Майкрософт"
+    },
+    {
+        eng: "This is Elon Musk, he is from Tesla",
+        rus: "Это Илон Маск, он из Тесла"
+    },
+    {
+        eng: "This is Jack Ma, he is from Ali Baba",
+        rus: "Это Джек Ма, он из Али Баба"
     }
-]
+]);

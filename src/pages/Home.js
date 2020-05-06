b7cd13002aedae50d67a27b2149550836c9aa289
nazimkazim <nazik_111@mail.ru>
2020-05-06 16:56:09 +0600

    add overflow-x property to table

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
-import { Link } from "react-router-dom";
 import Services from "../components/Services";
 import FeaturedFlowers from "../components/FeaturedLessons";
 

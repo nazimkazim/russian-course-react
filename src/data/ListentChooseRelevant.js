5fefc45d690900c8113af3e002d970a09ff0d1f2
nazimkazim <nazik_111@mail.ru>
2020-05-12 21:57:51 +0600

    add listen choose relevant activity

--- src/data/ListentChooseRelevant.js
+++ src/data/ListentChooseRelevant.js
@@ -1,3 +1,5 @@
+//Lesson 1.2
+
 // Robert fisk
 import RoberFisk from '../images/ListenChooseRelevant/Lesson1.2/audio/RobertFisk.wav';
 import RFIndependent from '../images/ListenChooseRelevant/Lesson1.2/images/independent.jpg';
@@ -27,6 +29,43 @@ import ElonMusk from '../images/ListenChooseRelevant/Lesson1.2/audio/ElonMusk.wa
 import elonmusk from '../images/ListenChooseRelevant/Lesson1.2/images/elonmusk.jpg';
 import spacex from '../images/ListenChooseRelevant/Lesson1.2/images/spacex.jpg';
 import tesla from '../images/ListenChooseRelevant/Lesson1.2/images/tesla.jpg';
+
+//Lesson 1.3
+// Merkel
+import AngelaMerkel from '../images/ListenChooseRelevant/Lesson1.3/Audio/merkel.wav';
+import angela_merkel from '../images/ListenChooseRelevant/Lesson1.3/Images/angela_merkel.jpg';
+import party from '../images/ListenChooseRelevant/Lesson1.3/Images/party.jpg';
+import einstein from '../images/ListenChooseRelevant/Lesson1.3/Images/einstein.jpg';
+
+// Khasan Shah
+import shash from '../images/ListenChooseRelevant/Lesson1.3/Audio/shash.wav';
+import footballer from '../images/ListenChooseRelevant/Lesson1.3/Images/footballer.jpg';
+import turkish_flag from '../images/ListenChooseRelevant/Lesson1.3/Images/turkish_flag.jpg';
+import fans from '../images/ListenChooseRelevant/Lesson1.3/Images/fans.jpg';
+
+// Mozart
+import mozart from '../images/ListenChooseRelevant/Lesson1.3/Audio/mozart.wav';
+import mozartPic from '../images/ListenChooseRelevant/Lesson1.3/Images/mozart.jpg';
+import austrian_flag from '../images/ListenChooseRelevant/Lesson1.3/Images/austrian_flag.jpg';
+import conductor from '../images/ListenChooseRelevant/Lesson1.3/Images/conductor.jpg';
+
+// cia_agent
+import cia_agent from '../images/ListenChooseRelevant/Lesson1.3/Audio/cia_agent.wav';
+import CIA from '../images/ListenChooseRelevant/Lesson1.3/Images/CIA.jpg';
+import russian_flag from '../images/ListenChooseRelevant/Lesson1.3/Images/russian_flag.jpg';
+import secret from '../images/ListenChooseRelevant/Lesson1.3/Images/secret.jpg';
+
+// klitchko
+import klitchko from '../images/ListenChooseRelevant/Lesson1.3/Audio/klitchko.wav';
+import boxing from '../images/ListenChooseRelevant/Lesson1.3/Images/boxing.jpg';
+import ukraine_flag from '../images/ListenChooseRelevant/Lesson1.3/Images/ukraine_flag.jpg';
+import klitchkoPic from '../images/ListenChooseRelevant/Lesson1.3/Images/klitchko.jpg';
+
+// mr & ms smith
+import mister_missis_smith from '../images/ListenChooseRelevant/Lesson1.3/Audio/mister_missis_smith.wav';
+import film from '../images/ListenChooseRelevant/Lesson1.3/Images/film.jpg';
+import killers from '../images/ListenChooseRelevant/Lesson1.3/Images/killers.jpg';
+import mr_and_missis_smith from '../images/ListenChooseRelevant/Lesson1.3/Images/mr_and_missis_smith.jpg';
 
 var _ = require('lodash');
 
@@ -121,5 +160,117 @@ export const ListenChooseRelevantSet1 = _.shuffle([
             }
         ],
         answers: ['elonmusk', 'spacex', 'tesla']
+    }
+]);
+
+
+export const ListenChooseRelevantSet2 = _.shuffle([
+    {
+        audio: AngelaMerkel,
+        picture: [
+            {
+                src: angela_merkel,
+                name: 'angela_merkel'
+            },
+            {
+                src: party,
+                name: 'party'
+            },
+            {
+                src: einstein,
+                name: 'einstein'
+            }
+        ],
+        answers: ['angela_merkel', 'party', 'einstein']
+    },
+    {
+        audio: shash,
+        picture: [
+            {
+                src: footballer,
+                name: 'footballer'
+            },
+            {
+                src: turkish_flag,
+                name: 'turkish_flag'
+            },
+            {
+                src: fans,
+                name: 'fans'
+            }
+        ],
+        answers: ['footballer', 'turkish_flag', 'fans']
+    },
+    {
+        audio: mozart,
+        picture: [
+            {
+                src: mozartPic,
+                name: 'mozart'
+            },
+            {
+                src: austrian_flag,
+                name: 'austrian_flag'
+            },
+            {
+                src: conductor,
+                name: 'conductor'
+            }
+        ],
+        answers: ['mozart', 'austrian_flag', 'conductor']
+    },
+    {
+        audio: cia_agent,
+        picture: [
+            {
+                src: CIA,
+                name: 'CIA'
+            },
+            {
+                src: russian_flag,
+                name: 'russian_flag'
+            },
+            {
+                src: secret,
+                name: 'secret'
+            }
+        ],
+        answers: ['CIA', 'russian_flag', 'secret']
+    },
+    {
+        audio: klitchko,
+        picture: [
+            {
+                src: klitchkoPic,
+                name: 'klitchkoPic'
+            },
+            {
+                src: boxing,
+                name: 'boxing'
+            },
+            {
+                src: ukraine_flag,
+                name: 'ukraine_flag'
+            }
+        ],
+        answers: ['klitchkoPic', 'boxing', 'ukraine_flag']
+    },
+    {
+        audio: mister_missis_smith,
+        picture: [
+            {
+                src: film,
+                name: 'film'
+            },
+            {
+                src: killers,
+                name: 'killers'
+            },
+            {
+                src: mr_and_missis_smith,
+                name: 'mr_and_missis_smith'
+            }
+        ],
+        answers: ['film', 'killers', 'mr_and_missis_smith']
     }
 ]);
--- src/pages/Lesson_1_3.js
+++ src/pages/Lesson_1_3.js
@@ -9,6 +9,8 @@ import { set3COSOC } from '../data/ComposeOrderedSentOnClick';
 import NewWordsBoard from '../components/NewWordsBoard';
 import { NWset2 } from '../data/newWords';
 import ObjectPronounChart from '../components/Charts/ObjectPronounsChart';
+import ListenChooseRelevant from '../components/listenChooseRelevant/AudioContainer';
+import { ListenChooseRelevantSet2 } from '../data/ListentChooseRelevant';
 
 
 
@@ -40,6 +42,35 @@ class Lesson_1_3 extends Component {
             </div>
           </div>
         </section>
+        <div className="newSection">
+          <SectionHeader name="Чтение и слушание" engName="Reading and listening" />
+          <div className="container is-fluid">
+            <div className="columns">
+              <div className="column">
+                <Instruction letter="a" name="Слушайте пять (5) аудио фрагмента, и для каждого аудиофрагмента выберите три (3) соответствующих изображения" engName="Listen to five (5) audio clips, and for each audio clip, select three (3) matching images" />
+                <ListenChooseRelevant data={ ListenChooseRelevantSet2 } />
+                <hr />
+                {/* <div className="columns">
+                  <div className={ `column ${this.state.hiden && 'hide'}` }>
+                    <Instruction letter="a" name="Читайте и слушайте тексты. Попытайтесь отгадать незнакомые слова" engName="Listen and read the texts. Try to guess meanings of the unknown words" />
+                    <ActivityContainer data={ set2TextQuizzer } />
+                  </div>
+                  <div className="column">
+                    <Instruction letter="b" name="Читайте на английском и постарайтесь сказать на русском" engName="Read English version and try to tell in Russian" />
+                    <button className="button is-info marginBottom" onClick={ this.hideHandler }>{ this.state.hiden ? 'Reveal left part' : 'Hide left part' }</button>
+                    <MemorizeOnClickActivity data={ MTCText1 } />
+                    <hr />
+                    <MemorizeOnClickActivity data={ MTCText2 } />
+                    <hr />
+                    <MemorizeOnClickActivity data={ MTCText3 } />
+                    <hr />
+                    <MemorizeOnClickActivity data={ MTCText4 } />
+                  </div>
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     );
   }

05d0f89c31311f73fc8df06ebbc55ce3f4945a49
nazimkazim <nazik_111@mail.ru>
2020-06-16 12:48:56 +0600

    add fix rotation

--- src/components/MemoryGame/Card.js
+++ src/components/MemoryGame/Card.js
@@ -1,19 +1,24 @@
 import React from 'react'
 import Corner from '../../images/MemoryGameAssets/corner.png'
 import Star from '../../images/MemoryGameAssets/star.png'
+import Glass from '../../images/MemoryGameAssets/glass.png'
 
 function Card() {
   return (
-    <div className="memory-game-card">
+    <div className="memory-game-card visible">
       <div className="memory-game-card-back memory-game-card-face">
-
+        <img className="corner corner-top-left" src={Corner} alt=""/>
+        <img className="corner corner-top-right" src={Corner} alt=""/>
+        <img className="corner corner-bottom-left" src={Corner} alt=""/>
+        <img className="corner corner-bottom-right" src={Corner} alt=""/>
+        <img className="star" src={Star} alt=""/>
       </div>
       <div className="memory-game-card-front memory-game-card-face">
         <img className="corner corner-top-left" src={Corner} alt=""/>
         <img className="corner corner-top-right" src={Corner} alt=""/>
         <img className="corner corner-bottom-left" src={Corner} alt=""/>
         <img className="corner corner-bottom-right" src={Corner} alt=""/>
-        <img className="star" src={Star} alt=""/>
+        <img className="memory-game-card-value" src={Glass} alt=""/>
       </div>
     </div>
   )

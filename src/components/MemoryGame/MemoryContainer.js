fbe794371e35d3aa7c1f0925675d09edd27ade03
nazimkazim <nazik_111@mail.ru>
2020-06-19 21:30:00 +0600

    add sound

--- src/components/MemoryGame/MemoryContainer.js
+++ src/components/MemoryGame/MemoryContainer.js
@@ -1,16 +1,21 @@
 import React, { useEffect, useState } from "react";
+import CountdownTimer from "react-component-countdown-timer";
 import Card from "./Card";
+import slide from "../../data/media/slide.wav";
+import notification from "../../data/media/notification.wav";
+import denied from "../../data/media/denied.mp3";
 
 function Index({ data }) {
   const [cards, setCards] = useState([]);
   const [pairs, setPairs] = useState([]);
-  
+  const [seconds, setSeconds] = useState(61);
+
   useEffect(() => {
-    const arr = data.map((item,index) => ({
+    const arr = data.map((item, index) => ({
       value: item.value,
       pic: item.pic,
       turned: false,
-      matched:false,
+      matched: false,
       index
     }));
     setCards(arr);
@@ -19,53 +24,88 @@ function Index({ data }) {
   useEffect(() => {
     if (pairs.length === 2) {
       if (pairs[0] === pairs[1]) {
-        const newCards = [...cards]
+        const newCards = [...cards];
         newCards.forEach((card) => {
           if (card.value === pairs[0]) {
-            card.matched = true
-          } 
-        })
-        setCards(newCards)
-        setPairs([])
+            card.matched = true;
+            setTimeout(() => {
+              let sound = new Audio(notification)
+              sound.play()
+            }, 500)
+            
+          }
+        });
+        setCards(newCards);
+        setPairs([]);
       } else {
         setTimeout(() => {
-          const newCards = [...cards]
+          const newCards = [...cards];
           newCards.forEach((card) => {
             if (card.value === pairs[0] || card.value === pairs[1]) {
-              card.turned = false
-            } 
-        })
-          setCards(newCards)
-          setPairs([])
-        }, 1000)
+              card.turned = false;
+            }
+          });
+          setCards(newCards);
+          setPairs([]);
+          let sound = new Audio(denied)
+          sound.play()
+        }, 1000);
       }
     }
     if (pairs.length > 2) {
 
     }
-  },[pairs,cards])
+  }, [pairs, cards]);
+  
 
-  
-  
+  var settings = {
+    count: seconds,
+    border: true,
+    showTitle: true,
+    noPoints: true,
+    hideDay: true,
+    hideHours: true
+  };
+
+
   return (
-    <div className="memory-game-container">
-      { cards &&
-        cards.map((card, index) => (
-          <div
-            onClick={ () => {
-              const newCards = [...cards];
-              if (!newCards[index].turned) {
-                newCards[index].turned = true;
-                setCards(newCards);
-                setPairs([...pairs, card.value])
-              }
-              
-            } }
-          >
-            <Card key={ index } item={ card } id={ index } />
-          </div>
-        )) }
+    <div>
+      
+      <div className="memory-game-info-container">
+        <div className="memory-game-info">
+          <span class="tag is-link is-large">
+            <CountdownTimer {...settings} />
+          </span>
+        </div>
+        <div className="memory-game-info">
+          <button className="button is-primary" onClick={() => {setSeconds(50)}}>start game</button>
+        </div>
+        <div className="memory-game-info">
+          <span class="tag is-link is-large">Flips</span>
+        </div>
+      </div>
+      <div className="memory-game-container">
+        { cards &&
+          cards.map((card, index) => (
+            <div
+              onClick={ () => {
+                const newCards = [...cards];
+                if (!newCards[index].turned) {
+                  let sound = new Audio(slide);
+                  sound.play()
+                  newCards[index].turned = true;
+                  setCards(newCards);
+                  setPairs([...pairs, card.value]);
+                }
+
+              } }
+            >
+              <Card key={ index } item={ card } id={ index } />
+            </div>
+          )) }
+      </div>
     </div>
+
   );
 }
 export default Index;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 import { LessonProvider } from "./context";
 import { BlogProvider } from "./contextBlog";
 
+
 ReactDOM.render(
   <LessonProvider>
     <BlogProvider>

bcb53888f4fcb30059764b7b58b93e59e15f5af8
nazimkazim <nazik_111@mail.ru>
2020-12-12 18:38:28 +0600

    add greeting

--- src/data/TextGenerator.js
+++ src/data/TextGenerator.js
@@ -1,5 +1,6 @@
 // Lesson 2.1
 export const generateText1 = () => {
+  const greeting = ['Привет', 'Здравствуйте', 'Добрый день']
   const name = ['Мария', 'Лаура', 'Надя', 'Игорь', 'Иван', 'Николай'];
   const place = ['России', 'Америки', 'Австралии', 'Франции', 'Казахстана', 'Италии'];
   const workPlace = ['ресторане', 'банке', 'школе', 'университете', 'парламенте', 'больнице', 'театре'];
@@ -11,5 +12,5 @@ export const generateText1 = () => {
   const generateNumber = (arr) => {
     return Math.floor(Math.random() * arr.length);
   };
-  return `Привет меня зовут <strong>${name[generateNumber(name)]}</strong>. Я из <strong>${place[generateNumber(place)]}</strong>. Я работаю в <strong>${workPlace[generateNumber(workPlace)]}</strong> <strong>${profession[generateNumber(profession)]}</strong>. Я очень люблю свою профессию. Вот мой типичный день. Я встаю <strong>${earlyOrLate[generateNumber(earlyOrLate)]}</strong>, а <strong>${sequence[generateNumber(sequence)]}</strong> я иду в душ, <strong>${sequence[generateNumber(sequence)]}</strong> я завтракаю, <strong>${sequence[generateNumber(sequence)]}</strong> я иду в офис, я обедаю в <strong>${placeToEat[generateNumber(placeToEat)]}</strong>, я работаю до <strong>${time[generateNumber(time)]}</strong> вечера, <strong>${sequence[generateNumber(sequence)]}</strong> я иду домой, я ужинаю в <strong>${placeToEat[generateNumber(placeToEat)]}</strong>, и <strong>${sequence[generateNumber(sequence)]}</strong> иду спать`;
+  return `<strong>${greeting[generateNumber(greeting)]}</strong> меня зовут <strong>${name[generateNumber(name)]}</strong>. Я из <strong>${place[generateNumber(place)]}</strong>. Я работаю в <strong>${workPlace[generateNumber(workPlace)]}</strong> <strong>${profession[generateNumber(profession)]}</strong>. Я очень люблю свою профессию. Вот мой типичный день. Я встаю <strong>${earlyOrLate[generateNumber(earlyOrLate)]}</strong>, а <strong>${sequence[generateNumber(sequence)]}</strong> я иду в душ, <strong>${sequence[generateNumber(sequence)]}</strong> я завтракаю, <strong>${sequence[generateNumber(sequence)]}</strong> я иду в офис, я обедаю в <strong>${placeToEat[generateNumber(placeToEat)]}</strong>, я работаю до <strong>${time[generateNumber(time)]}</strong> вечера, <strong>${sequence[generateNumber(sequence)]}</strong> я иду домой, я ужинаю в <strong>${placeToEat[generateNumber(placeToEat)]}</strong>, и <strong>${sequence[generateNumber(sequence)]}</strong> иду спать`;
 };

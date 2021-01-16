4c043ec4b3415ff51c7868951ffb62ce679600a6
nazimkazim <nazik_111@mail.ru>
2021-01-16 17:30:37 +0600

    add grouping sets

--- src/components/DragAndDrop/Board.js
+++ src/components/DragAndDrop/Board.js
@@ -1,26 +1,44 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
+const Root = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  width: 100%;
+  max-width: 300px;
+  height: 100%;
+  background-color: #28151E;
+  padding: 15px;
+  overflow:'scroll';
+  margin-left: 5px;
+  margin-right: 5px;
+  -webkit-box-shadow: -5px 0px 3px -2px rgba(46, 33, 46, 1);
+  -moz-box-shadow: -5px 0px 3px -2px rgba(46, 33, 46, 1);
+  box-shadow: -5px 0px 3px -2px rgba(46, 33, 46, 1);
+  border-radius: 3px;
+`
 function Board(props) {
-  const [cards, setCards] = useState([]);
+  // const [cards, setCards] = useState([]);
+  // console.log({cards}, props.set);
 
-  console.log(cards);
   const drop = e => {
     e.preventDefault();
     const card_id = e.dataTransfer.getData('card_id');
     const card = document.getElementById(card_id);
     card.style.display = 'block';
     e.target.appendChild(card);
-    setCards([...cards, card_id]);
+    // setCards([...cards, card_id]);
+    props.handleCardAppend(card_id);
   };
 
   const dragOver = e => {
     e.preventDefault();
   };
   return (
-    <div id={ props.id } onDrop={ drop } onDragOver={ dragOver } className={ props.className }>
+    <Root id={ props.id } onDrop={ drop } onDragOver={ dragOver } className={ props.className }>
       {props.children }
-    </div>
+    </Root>
   );
 }
 
--- src/components/DragAndDrop/Container.js
+++ src/components/DragAndDrop/Container.js
@@ -6,11 +6,35 @@ import './styles.css';
 const _ = require('lodash');
 
 function Container({ data, name1, name2, img1, img2 }) {
+  const [formattedArr, setFormattedArr] = useState([]);
+
+  const [ males, setMales ] = useState([]);
+  const [ females, setFemales ] = useState([]);
+
+  const handleAppendToMales = (cardId) => {
+    setMales((prevCards) => [...prevCards, cardId]);
+    setFemales((prevCards) => prevCards.filter((card) => card !== cardId));
+  }
+
+  const handleAppendToFemales = (cardId) => {
+    setFemales((prevCards) => [...prevCards, cardId]);
+    setMales((prevCards) => prevCards.filter((card) => card !== cardId));
+  }
+
+  const handleAppendToCardsHolder = (cardId) => {
+    setFemales((prevCards) => prevCards.filter((card) => card !== cardId));
+    setMales((prevCards) => prevCards.filter((card) => card !== cardId));
+  }
+
+  useEffect(() => {
+    console.log({ males, females, formattedArr })
+  }, [males, females])
+ 
+  console.log(data);
   const set1 = Object.keys(data)[0]
   const set2 = Object.keys(data)[1]
   //console.log(set1);
   //console.log(Object.keys(data));
-  const [formattedArr, setFormattedArr] = useState([]);
   useEffect(() => {
     let arr = [];
     arr.push(data["set1"]);
@@ -20,17 +44,17 @@ function Container({ data, name1, name2, img1, img2 }) {
   //console.log(formattedArr);
   return (
     <div className="flexbox">
-      <Board id="board-1" className="board">
+      <Board id="board-1" handleCardAppend={handleAppendToCardsHolder} overflowed={true}>
         { formattedArr.map(item => (
           <Card id={item} className="card" draggable="true">
             <p>{item}</p>
           </Card>
         )) }
       </Board>
-      <Board id="board-2" className="board">
+      <Board id="board-2" handleCardAppend={handleAppendToMales} >
         <Plate name={name1} img={img1}/>
       </Board>
-      <Board id="board-3" className="board">
+      <Board id="board-3" handleCardAppend={handleAppendToFemales} >
       <Plate name={name2} img={img2}/>
       </Board>
     </div>

b57e11a96ee20cc170ca137fa7a2aef0a3f4e0b8
nazimkazim <nazik_111@mail.ru>
2021-01-22 19:54:26 +0600

    add implement select radio bix

--- src/components/SentenceWithOptions/Index.js
+++ src/components/SentenceWithOptions/Index.js
@@ -1,58 +1,103 @@
 import React from 'react';
 import styled from 'styled-components';
+import './styles.css';
 
 const Root = styled.div`
   display:flex;
+  flex-direction:column;
+  margin:0 auto;
+  max-width:420px;
+  height:320px;
+  min-height:300px;
+  /* border:2px solid #2a9d8f; */
+  border-radius:8px;
 `;
+
+const ContentHolder = styled.div`
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  /* background-color:blue; */
+  width:100%;
+  height:80%;
+`;
+
+const ButtonHolder = styled.div`
+  display:flex;
+  /* background-color:red; */
+  align-items:center;
+  width:100%;
+  height:20%;
+  border-top:1px solid black;
+`;
+
+const Label = styled.label`
+  cursor:pointer;
+  background-color:${props => props.checkedItem ? '#2a9d8f' : ''};
+  padding:5px;
+  color:${props => props.checkedItem ? 'white' : ''};
+  border:2px solid ${props => props.checkedItem ? '#264653' : '#d8e2dc'};
+  border-bottom:3px solid #264653;
+  margin-right:3px;
+  font-size:18px;
+`;
+
+const Phrase = styled.div`
+  cursor:pointer;
+  padding:5px;
+  border:2px solid #d8e2dc;
+  margin-right:3px;
+  font-size:18px;
+`
+
 
 function Index({ data }) {
   //console.log(data);
+  const [option1, setOption1] = React.useState([]);
+  const [option2, setOption2] = React.useState([]);
 
-  const [answer, setAnswer] = React.useState([]);
-
-  const chooseOption = (e, option) => {
-    console.log('chosen', option);
-    let optionName = e.target.name;
-
-    /* if (optionName === 'option1') {
-      setAnswer(prevState => [...prevState, {
-        status: option.status,
-        id: option.id
-      }]);
-    } */
-    /* if (optionName === 'option1') {
-      data[0].options1.forEach(item => {
-        if (item.id === option.id) {
-          console.log('success');
-        }
-      })
-    } */
+  //console.log(answer);
+  const checkAnswer = () => {
+    const option1Ans = data[0].options1.find(({ id }) => id === option1);
+    const status1 = option1Ans && option1Ans.status;
+    const option2Ans = data[0].options2.find(({ id }) => id === option2);
+    const status2 = option2Ans && option2Ans.status;
+    if (status1 && status2) {
+      console.log('you won');
+    }
   };
-  console.log(answer);
+
+
   return (
-    <div>
-      {
-        <Root>
-          <div>{ data[0].start }</div>
-          {/* <div onClick={ () => chooseOption(data[0].options1.option1) }>{ data[0].options1.option1.word }</div> /
-          <div onClick={ () => chooseOption(data[0].options1.option2) }>{ data[0].options1.option2.word }</div> */}
-          <div class="control">
-            <label class="radio">
-              <input type="radio" name="option1" onChange={ (e) => chooseOption(e, data[0].options1.option1) } checked={data[0].options1.option1.checked} />
-              { data[0].options1.option1.word }
-            </label>
-            <label class="radio">
-              <input type="radio" name="option1" onChange={ (e) => chooseOption(e, data[0].options1.option2) } checked={data[0].options1.option1.checked} />
-              { data[0].options1.option2.word }
-            </label>
-          </div>
-          <div>{ data[0].middle }</div>
-          <div>{ data[0].options2.option1.word }</div> /
-          <div>{ data[0].options2.option2.word }</div>
-          <div>{ data[0].end }</div>
-        </Root>
-      }
-    </div>
+    <Root>
+      <ContentHolder>
+        <Phrase>{ data[0].start }</Phrase>
+        <div class="control">
+          { data[0].options1.map(option => {
+            //console.log(option1 === option.id);
+            return (
+              <Label checkedItem={ option1 === option.id }>
+                <input type="radio" name="option1" onChange={ (e) => setOption1(option.id) } checked={ option1 === option.id } />
+                { option.word }
+              </Label>
+            );
+          }) }
+        </div>
+        <Phrase>{ data[0].middle }</Phrase>
+        { data[0].options2.map(option => (
+          <Label checkedItem={ option2 === option.id }>
+            <input type="radio" name="option2" onChange={ (e) => setOption2(option.id) } checked={ option2 === option.id } />
+            { option.word }
+          </Label>
+        )) }
+        <Phrase>{ data[0].end }</Phrase>
+      </ContentHolder>
+      <ButtonHolder>
+
+        <button className="button" onClick={ checkAnswer }>Check</button>
+      </ButtonHolder>
+    </Root>
+
   );
 }
 
--- src/data/ChooseOption.js
+++ src/data/ChooseOption.js
@@ -3,35 +3,34 @@ import { v4 as uuidv4 } from 'uuid';
 export const chooseOptionSet1 = [
   {
     start: 'Это',
-    options1: {
-      option1: {
+    options1:
+      [{
         word: 'мой',
         id: uuidv4(),
         status: false,
-        checked:false
+        checked: false
       },
-      option2: {
+      {
         word: 'моя',
         id: uuidv4(),
         status: true,
-        checked:false
+        checked: false
       }
-    },
+      ],
     middle: 'мама',
-    options2: {
-      option1: {
-        word: 'Её',
-        id: uuidv4(),
-        status: true,
-        checked:false
-      },
-      option2: {
-        word: 'Его',
-        id: uuidv4(),
-        status: false,
-        checked:false
-      }
+    options2: [{
+      word: 'Её',
+      id: uuidv4(),
+      status: true,
+      checked: false
     },
+    {
+      word: 'Его',
+      id: uuidv4(),
+      status: false,
+      checked: false
+    }
+    ],
     end: 'зовут Юлия'
   }
 ];

ed6ebe329ceb3345f743fb859d761d96db4f0736
nazimkazim <nazik_111@mail.ru>
2021-02-06 18:44:30 +0600

    add design activity

--- src/components/CVTemplate/Index.js
+++ src/components/CVTemplate/Index.js
@@ -1,15 +1,177 @@
-import React from 'react'
-import {generateCV} from './data'
+import React from 'react';
+import { generateCV } from './data';
+import styled from 'styled-components'
+import PersonIcon from '@material-ui/icons/Person';
+import PlaceIcon from '@material-ui/icons/Place';
+import HomeIcon from '@material-ui/icons/Home';
+import EventIcon from '@material-ui/icons/Event';
+import RowingIcon from '@material-ui/icons/Rowing';
+
+const Root = styled.div`
+  display:flex;
+  justify-content:space-between;
+  margin:0 auto;
+  padding:6px;
+  max-width:800px;
+  min-height:300px;
+  background-color:#FE5722;
+  border-radius:6px;
+`
+
+const Side = styled.div`
+  position:relative;
+  display:flex;
+  flex-direction:column;
+  min-height:100%;
+  justify-content:${props => props ? props.verticalAlignment : 'center'};
+  align-items:center;
+  flex:0.49;
+  background-color:#FE440B;
+  border-radius:6px;
+  border-top:3px solid #FFE7DF;
+  -webkit-box-shadow: -13px 14px 10px -12px rgba(223,53,1,1);
+  -moz-box-shadow: -13px 14px 10px -12px rgba(223,53,1,1);
+  box-shadow: -13px 14px 10px -12px rgba(223,53,1,1);
+`
+
+const Avatar = styled.div`
+  width:200px;
+  height:200px;
+  border-radius:50%;
+  border:1px solid #FFE7DF;
+  background-image:${props => `url(${props.avatar})`};
+  background-repeat: no-repeat;
+  background-size:cover;
+  background-position: center;
+  margin-top:20px;
+  -webkit-box-shadow: -13px 14px 10px -12px rgba(223,53,1,1);
+  -moz-box-shadow: -13px 14px 10px -12px rgba(223,53,1,1);
+  box-shadow: -13px 14px 10px -12px rgba(223,53,1,1);
+  cursor:pointer;
+`
+
+const Header = styled.h3`
+  color:white;
+  font-size: ${props => props.fontSize};
+  text-transform:${props => props.stringCase};
+  font-weight:bold;
+  margin-top:${props => props.marginTop};
+  cursor:pointer;
+  margin-bottom:${props => props.marginBottom};
+`
+
+const Emoji = styled.span`
+  font-size:20px;
+`
+
+const SideHeader = styled.div`
+  position:absolute;
+  left:0;
+  top:0;
+  width:100%;
+  min-height:10px;
+  /* background-color:blue; */
+  padding-left:10px;
+  padding-top:10px;
+  display:flex;
+  align-items:center;
+  color:white;
+  font-weight:bold;
+`
+
+const Info = styled.div`
+  width:100%;
+  margin-top:50px;
+  min-height:100px;
+  /* background-color:blue; */
+`
+
+const ListItem = styled.div`
+  display:flex;
+  width:100%;
+  min-height:24px;
+  height:3px;
+  /* background-color:yellow; */
+  margin-top:10px;
+`
+
+const ListItemHalf = styled.div`  
+  display:flex;
+  flex-direction:${props => props.flexDirection ? 'column' : 'row'};
+  align-items:center;
+  flex:0.5;
+  min-height:100%;
+  font-weight:${props => props.fontWeight};
+  padding-left:${props => props.paddingLeft};
+  color:white;
+  /* background-color:red; */
+  padding-right:2px;
+`
+
+const Icon = styled.div`
+  cursor:pointer;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  padding:14px;
+  width:15px;
+  height:15px;
+  background-color:white;
+  border-radius:50%;
+  color:#FE5722;
+  margin-right:10px;
+  margin-left:5px;
+`
 
 function Index() {
+  const [data, setData] = React.useState('')
   React.useEffect(() => {
-    generateCV()
-  }, [])
+    setData(generateCV())
+  }, []);
+
+  console.log(data);
   return (
-    <div>
-      CV template
-    </div>
-  )
+    <Root>
+      <Side>
+        <Avatar avatar={data.avatar}/>
+        <Header fontSize="24px" stringCase="uppercase" marginTop="20px">
+          {data.name}
+        </Header>
+        <Header fontSize="14px" marginBottom="40px">
+          {data.occupation} {data.likeOrNot === 'no' ? <Emoji>😞</Emoji> : <Emoji>😃</Emoji>}
+        </Header>
+      </Side>
+      <Side verticalAlignment="start">
+        <SideHeader>
+          Профайл
+        </SideHeader>
+        <Info>
+          <ListItem>
+            <ListItemHalf fontWeight="bold"><Icon><HomeIcon/></Icon>Месторождение</ListItemHalf>
+            <ListItemHalf paddingLeft="10px" fontWeight="normal">: {data.birthPlace}</ListItemHalf>
+          </ListItem>
+          <ListItem>
+            <ListItemHalf fontWeight="bold"><Icon><PlaceIcon/></Icon>Местопроживание</ListItemHalf>
+            <ListItemHalf paddingLeft="15px" fontWeight="normal">: {data.currentPlace}</ListItemHalf>
+          </ListItem>
+          <ListItem>
+            <ListItemHalf fontWeight="bold"><Icon><EventIcon/></Icon>Возраст</ListItemHalf>
+            <ListItemHalf paddingLeft="10px" fontWeight="normal">: {data.age}</ListItemHalf>
+          </ListItem>
+          <ListItem>
+            <ListItemHalf fontWeight="bold"><Icon><RowingIcon/></Icon>Хобби</ListItemHalf>
+            <ListItemHalf paddingLeft="10px" fontWeight="normal">: {data.hobbies && data.hobbies.map(item => (
+              <p>{item}</p>
+            ))}</ListItemHalf>
+          </ListItem>
+          <ListItem>
+            <ListItemHalf fontWeight="bold"><Icon><PersonIcon/></Icon>Месторождение</ListItemHalf>
+            <ListItemHalf paddingLeft="10px" fontWeight="normal">: {data.birthPlace}</ListItemHalf>
+          </ListItem>
+        </Info>
+      </Side>
+    </Root>
+  );
 }
 
-export default Index
+export default Index;
--- src/components/CVTemplate/data.js
+++ src/components/CVTemplate/data.js
@@ -1,4 +1,7 @@
 const _ = require('lodash');
+const malePhoto = 'https://res.cloudinary.com/nzmai/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1612604229/russian%20course/general/ben-parker-OhKElOkQ3RE-unsplash.jpg';
+const femalePhoto = 'https://res.cloudinary.com/nzmai/image/upload/w_1000,c_fill,ar_1:1,g_auto,r_max,bo_5px_solid_red,b_rgb:262c35/v1612604225/russian%20course/general/sigmund-jzz_3jWMzHA-unsplash.jpg';
+
 export const generateCV = () => {
   const genderType = ["male", "female"];
   const yesOrNo = ['yes', 'no'];
@@ -9,7 +12,7 @@ export const generateCV = () => {
   const currentCountries = ['Казахстан', 'Америка', 'Украина', 'Литва', 'Мексика', 'Аргентина', 'Узбекистан', 'Польша'];
   const occupations = ['Менеджер', 'Президент', 'Бухгалтер', 'Профессор', 'Футболист', 'Депутат', 'Учитель', 'Доктор', 'Дизайнер'];
   const skills = ['Майкрософт Офис', 'Эксэль', 'Фотошоп', 'Маркетинг', 'Планирование', 'Тайм-менеджмент', 'Проджект менеджмент'];
-  const hobbies = ['Фотографирование', 'Дайвинг', 'Велоспорт', 'Сноубординг', 'Каратэ', 'Гитару', 'Пианино', 'Музыку', 'Футбол', 'Баскетбол'];
+  const hobbies = ['Фотографирование', 'Дайвинг', 'Велоспорт', 'Сноубординг', 'Каратэ', 'Гитара', 'Пианино', 'Музыка', 'Футбол', 'Баскетбол'];
   const languages = ['Русский', 'Итальянский', 'Испанский', 'Английский', 'Французский', 'Китайский', 'Турецкий', 'Японский'];
 
   const chosenGender = genderType[generateChoiceNum(genderType.length)];
@@ -23,10 +26,10 @@ export const generateCV = () => {
   const chosenHobbies = _.shuffle(hobbies).slice(0, 5);
   const chosenlanguages = _.shuffle(languages).slice(0, 4);
   const generatedAge = randomRange(23, 63);
-  console.log(chosenGender, chosenYesOrNo);
 
   if (chosenGender === "female") {
     return {
+      avatar: femalePhoto,
       gender: chosenGender,
       name: chosenFemaleName,
       birthPlace: chosenBirthPlace,
@@ -37,11 +40,12 @@ export const generateCV = () => {
       hobbies: chosenHobbies,
       skills: chosenSkills,
       languages: chosenlanguages
-    }
+    };
   }
 
   if (chosenGender === "male") {
     return {
+      avatar: malePhoto,
       gender: chosenGender,
       name: chosenMaleName,
       likeOrNot: chosenYesOrNo,
@@ -52,7 +56,7 @@ export const generateCV = () => {
       skills: chosenSkills,
       hobbies: chosenHobbies,
       languages: chosenlanguages
-    }
+    };
   }
 };
 
